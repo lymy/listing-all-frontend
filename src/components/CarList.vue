36081bc556823995ba737bc5e6696e106587cbbf
Halimi <halimi@Halimis-MacBook-Pro.local>
2019-12-07 22:06:45 +0800

    first commit

---
+++ src/components/CarList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="page-header">
+      <h2 class="title">Cars available for rent</h2>
+    </div>
+    <div class="content columns is-multiline">
+      <div v-for="car in cars" v-bind:key="car.id" class="column is-one-quarter">
+        <div class="card">
+          <div class="card-image">
+            <img v-bind:src="car.image">
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-size-4">{{ car.name }} <span class="is-size-6 has-text-gray"> {{ car.category }}</span></p>
+              </div>
+            </div>
+            <div class="content">
+              {{ car.price }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .page-header {
+    margin-bottom: 30px;
+  }
+</style>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'CarList',
+  data() {
+    return { cars: []}
+  },
+  methods: {
+    async getAllCars() {
+      const response = await axios.get('http://localhost:8000/cars');
+      this.cars = response.data;
+    }
+  },
+  mounted() {
+    this.getAllCars();
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<CarList />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CarList from '@/components/CarList.vue'
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld
+    CarList
   }
 }
 </script>
